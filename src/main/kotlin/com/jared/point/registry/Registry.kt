e35afe6c0647cd3510eb53131694318a3c42903e
kevin <kevin.vandenbreemen@gmail.com>
2022-05-31 14:45:38 -0700

    Make this thread-safe

--- src/main/kotlin/com/jared/point/registry/Registry.kt
+++ src/main/kotlin/com/jared/point/registry/Registry.kt
@@ -1,16 +1,28 @@
 package com.jared.point.registry
 
+import java.util.concurrent.Semaphore
+
 class Registry {
+
+    private val semaphore = Semaphore(1)
 
     private val _items: MutableList<RegistryItem> = mutableListOf()
     val items: List<RegistryItem> get() = _items.toList()
 
     fun add(item: RegistryItem): Boolean {
-        if(_items.contains(item)) {
+        if(!semaphore.tryAcquire()){
             return false
         }
-        this._items.add(item)
-        return true
+
+        try {
+            if (_items.contains(item)) {
+                return false
+            }
+            this._items.add(item)
+            return true
+        } finally {
+            semaphore.release()
+        }
     }
 
 
