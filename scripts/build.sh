be7a1c57c5239cf3f189a5dadfab2a4415f214ea
kevin <kevin.vandenbreemen@gmail.com>
2022-06-16 16:12:53 -0700

    Just put the containers on the network directly instead of adding them later

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,9 +8,5 @@ echo "Network setup (this might fail if network already setup)"
 docker network create jared
 
 echo "Starting docker containers"
-docker run --name point_registry -d jared_reg
-docker run --name jared_point -d jared_point
-
-echo "Connecting containers to registry"
-docker network connect jared point_registry
-docker network connect jared jared_point
+docker run --name point_registry -d --network="jared" jared_reg
+docker run --name jared_point -d --network="jared" jared_point
